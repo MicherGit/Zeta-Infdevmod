5d504248ab8bab51599f7c3ab8a071bf5d856e97
MicherGit <40839581+MicherGit@users.noreply.github.com>
2021-03-04 17:19:39 -0500

    gomsoggmpisfdngf

--- src/main/java/net/textilemc/examplemod/ExampleMod.java
+++ src/main/java/net/textilemc/examplemod/ExampleMod.java
@@ -1,8 +1,21 @@
 package net.textilemc.examplemod;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.block.Block;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 
 public class ExampleMod implements ModInitializer {
+	public final File file = new File("test.txt");
+	public final BufferedReader br = new BufferedReader(new FileReader(file));
+	public final String modid = "concernedconcern";
+
+	public ExampleMod() throws FileNotFoundException {
+	}
+
 	@Override
 	public void onInitialize() {
 		System.out.println("Hello Fabric world!");
